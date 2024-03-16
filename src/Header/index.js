e10991b5a2659bb9fdda6b9f9ff225cb46398dfa
Maksymilian555 <gozdek.maks@gmail.com>
2024-03-16 11:10:22 +0100

    Nav added and fixed

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,20 +1,22 @@
 import "./style.css";
+import logo from "../Images/icon.png"
 
 const Header = (props) => (
     <div className = "header">
-        <nav className = "nav">
-            <ul className = "navList">
-                <li>
-                    <button className = "navButton">Galeria</button>
+        <img src={logo} alt="logo" className = "nav_logo"></img>
+        <nav className = "header_nav">
+            <ul className = "nav_list">
+                <li className = "nav_case">
+                    <button className="nav_button">Galeria</button>
                 </li>
-                <li>
-                    <button className = "navButton">Kontakt</button>
+                <li className = "nav_case">
+                    <button className="nav_button">Kontakt</button>
                 </li>
-                <li>
-                    <button className = "navButton">O nas</button>
+                <li className = "nav_case">
+                    <button className="nav_button">O nas</button>
                 </li>
-                <li>
-                    <button className = "navButton">Projekty</button>
+                <li className = "nav_case">
+                    <button className="nav_button">Projekty</button>
                 </li>
             </ul>
         </nav>
