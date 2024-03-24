e1adf85f6878678f85404fb7168e7f86dddd859b
Maksymilian555 <gozdek.maks@gmail.com>
2024-03-24 16:25:46 +0100

    Customization in footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -3,10 +3,22 @@ import "./style.css";
 const Footer = () => (
     <section className="footer">
         <div className="footer_main">
-
+            <p>
+                <h4>Krótko o nas</h4>
+                <p>bla bla</p>
+            </p>
+            <p>
+                (Mapa strony)
+            </p>
+            <p>
+                <h4>Kontakt</h4>
+                <p>DC: BLA BLA  </p>
+            </p>   
         </div>
         <div className="footer_info">
-
+            <p>
+                Copyright Maksymilian Gozdek©
+            </p>
         </div>
     </section>
 )
